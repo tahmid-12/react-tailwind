230ea551b788a76656859c6ea03182b4248d61e3
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2023-08-27 20:57:31 +0600

    Cards Created

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,10 +2,12 @@ import Navbar from "./Navbar";
 import Hero from "./Hero";
 import Analytics from "./Analytics";
 import Newsletter from "./Newsletter";
+import Cards from "./Cards";
 
 export {
     Navbar,
     Hero,
     Analytics,
-    Newsletter
+    Newsletter,
+    Cards
 }
