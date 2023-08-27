9c28e5f5e39646b88614ce21b4e7927c8b451626
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2023-08-27 19:24:05 +0600

    hero component created

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     return (
         <>
             <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
-                <h1 className="w-full text-3xl font-bold text-[#00df9a]">Navbar</h1>
+                <h1 className="w-full text-3xl font-bold text-[#00df9a]">React Tailwind</h1>
                 <ul className="hidden md:flex ">
                     <li className="p-4">Company</li>
                     <li className="p-4">Home</li>
@@ -27,7 +27,7 @@ const Navbar = () => {
                     }
                 </div>
                 <div className={!open ? 'fixed left-[-100%] ease-in-out duration-300 md:hidden' : 'fixed left-0 top-0 w-[80%] h-full border-r border-r-gray-600 bg-[#000300] ease-in-out duration-300 md:hidden'}>
-                    <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">Navbar</h1>
+                    <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">React Tailwind</h1>
                     <ul className="flex flex-col uppercase p-4">
                         <li className="p-4 border-b border-gray-600">Company</li>
                         <li className="p-4 border-b border-gray-600">Home</li>
